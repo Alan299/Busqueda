e0e01a3e517d84e833e3ad72074935d6f535ffc0
Alan299 <luismooth.criminal@gmail.com>
2016-04-19 23:42:16 -0500

    Mejora La Búsqueda Separando la Cadena en Palabras.

--- economiaCompartida/src/java/beans/ConsultarBean.java
+++ economiaCompartida/src/java/beans/ConsultarBean.java
@@ -5,22 +5,18 @@
  */
 package beans;
 
+import java.util.*;
 import java.util.ArrayList;
-
+import java.util.Arrays;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpServletRequest;
 
-import logic.ConsultarC;
-import model.Publicacion;
 
-<<<<<<< HEAD
 import logic.ConsultarC;
 import model.Publicacion;
-=======
->>>>>>> origin/master
 /**
  *
  * @author Alan
@@ -28,33 +24,24 @@ import model.Publicacion;
 @ManagedBean
 @RequestScoped
 public class ConsultarBean {
-<<<<<<< HEAD
-=======
-
-    //private int elementoActual;
-
->>>>>>> origin/master
     private String clave;
     private ConsultarC termino;
     private ArrayList<Publicacion> resultados;
-
+   
+    
+    
     private final HttpServletRequest httpServletRequest; // Obtiene información de todas las peticiones de usuario.
     private final FacesContext faceContext; // Obtiene información de la aplicación
     private FacesMessage message; // Permite el envio de mensajes entre el bean y la vista.
-<<<<<<< HEAD
   
    
     public ConsultarBean(){
        
-=======
-    //private ConsultarC helper;
-
-    public ConsultarBean() {
->>>>>>> origin/master
         faceContext = FacesContext.getCurrentInstance();
         httpServletRequest = (HttpServletRequest) faceContext.getExternalContext().getRequest();
+
+        
     }
-<<<<<<< HEAD
     
     
     public String buscar(){
@@ -64,39 +51,26 @@ public class ConsultarBean {
        this.resultados = (ArrayList<Publicacion>) termino.buscar(clave);
        
        return "ConsultarIH";
-=======
-
-    public String buscar() {
-        termino = new ConsultarC();
-        this.resultados = new ArrayList<>();
-        System.out.println("clave: " + clave);
-        this.resultados = (ArrayList<Publicacion>) termino.buscar(clave);
-        //System.out.println("Tamaño de la lista de re: " + this.resultados.size());
-        return "ConsultarIH";
->>>>>>> origin/master
     }
-
-    public ConsultarC getTermino() {
+    public ConsultarC getTermino(){
         return termino;
     }
-
-    public void setTermino(ConsultarC t) {
+    public void setTermino(ConsultarC t){
         this.termino = t;
     }
-
-    public String getClave() {
+     public String getClave(){
         return clave;
     }
-
-    public void setClave(String c) {
+    public void setClave(String c){
         this.clave = c;
     }
-
-    public ArrayList<Publicacion> getResultados() {
+    
+  
+     public ArrayList<Publicacion> getResultados() {
         return this.resultados;
     }
-
-    public void setClave(ArrayList<Publicacion> r) {
+     
+      public void setClave(ArrayList<Publicacion> r){
         this.resultados = r;
     }
 }
--- economiaCompartida/src/java/logic/ConsultarC.java
+++ economiaCompartida/src/java/logic/ConsultarC.java
@@ -1,55 +1,75 @@
 /*
- * Clase diseñada como controlador para realizar busquedas.
- */
-package logic;
+    Clase diseniada como controlador pararealizar busquedas.
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Date;
-import java.util.List;
+*/
+package logic;
 
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
-
 import model.Publicacion;
+import java.util.*;
 import model.Usuario;
+
+
+
 
 /**
  *
  * @author Alan
  */
 public class ConsultarC {
-
-    private ArrayList<Publicacion> resultados;
+    
+    private ArrayList  <Publicacion> resultados;
     private Session session;
-
-    public ConsultarC() {
-        session = HibernateUtil.getSessionFactory().getCurrentSession();
-          //init();
+  
+  
+  
+     public ConsultarC(){
+          session = HibernateUtil.getSessionFactory().getCurrentSession();
+         
+       
+ 
     }
-
-    public void init() {
-        Usuario u = new Usuario();
-        this.resultados = new ArrayList();
-        Date d = new Date();
-        resultados.add(new Publicacion(1, u, "Oaxaca", "Mmemoria ram", d));
-        resultados.add(new Publicacion(2, u, "Oaxaca", "Mmemoria2 ram", d));
-        resultados.add(new Publicacion(3, u, "Oaxaca", "Mmemoria3 ram", d));
+    
+    
+    /**
+     * Método que obtiene las palabras de una cadena y crea unaexpresión regular para buscar coincidencias con todas las palabras.
+     * @param cadena Cadena a separar por palabras.
+     * @return La consulta p
+     */
+    public static String obtenerPalabras(String cadena){
+        if(cadena.length()<=0)
+            return cadena;
+        cadena = cadena.toLowerCase(); //Transforma la cadena a minúsculas.
+        String [] palabras  = cadena.split(" ");
+        String resultado = ".*(";
+        for(String p: palabras){
+            resultado+= p+"|";
+            
+        }
+        if(resultado.length() >= 5){
+            resultado =    resultado.substring(0,resultado.length()-1);
+        }
+        
+        resultado += ").*";
+        return resultado;
+     
+        
     }
-<<<<<<< HEAD
     
    
     
     
      public List<Publicacion> buscar(String clave ){
-         clave = clave.toLowerCase();
+         
+         clave = obtenerPalabras(clave);
          session = HibernateUtil.getSessionFactory().getCurrentSession();
          List<Publicacion> r = new ArrayList<>();
           try{
             Transaction tx = session.beginTransaction();
              Query q = session.createSQLQuery("select * from publicacion where "
-            + "LOWER(publicacion.descripcion) like :clave ; ").addEntity(Publicacion.class).setString("clave", "%" + clave + "%");
+            + "LOWER(publicacion.descripcion) ~ :clave ; ").addEntity(Publicacion.class).setString("clave",  clave );
 
             
             resultados = (ArrayList<Publicacion>) q.list();
@@ -58,41 +78,31 @@ public class ConsultarC {
             
            
         }catch (Exception e) {
-=======
-
-    public List<Publicacion> buscar(String clave) {
-        session = HibernateUtil.getSessionFactory().getCurrentSession();
-        List<Publicacion> r = new ArrayList<>();
-        try {
-            Transaction tx = session.beginTransaction();
-            Query q = session.createSQLQuery("select * from publicacion where publicacion.descripcion like :clave")
-                             .addEntity(Publicacion.class)
-                             .setString("clave", "%" + clave + "%");
-
-            resultados = (ArrayList<Publicacion>) q.list();
-            session.getTransaction().commit();
-            //this.ordenar();
-            System.out.println("Tamano de la lista de resultados: " + this.resultados.size());
-
-        } catch (Exception e) {
->>>>>>> origin/master
             e.printStackTrace();
             session.getTransaction().rollback();
         }
-        return resultados;
-
-    }
-
-    public void ordenar() {
+         return resultados;
+         
+     }
+    
+    public void ordenar(){
+        
         Collections.sort(this.resultados);
+        
     }
-
-    public ArrayList<Publicacion> getResultados() {
+   
+  
+     public ArrayList<Publicacion> getResultados(){
         return this.resultados;
     }
-
-    public void setResultados(ArrayList<Publicacion> r) {
-        this.resultados = r;
+    public void setResultados(ArrayList<Publicacion> r){
+        
+        this.resultados  = r;
     }
 
+
+     
+            
+            
 }
+
